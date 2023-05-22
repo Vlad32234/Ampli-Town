16698b92600d8932e1d0102a49df7e2c71cb1c2a
Vlad32234 <127133099+Vlad32234@users.noreply.github.com>
2023-05-22 21:14:16 +0300

    Task_13
    Task 13

---
+++ Task_13/P_1/script.js
@@ -0,0 +1,6 @@
+alert("Hello my name is Vlad!");
+document.write("Hello");
+document.write('Who are you?');
+answer = prompt("Where do you live?", "default answer");
+console.log(answer);
+alert(answer);
